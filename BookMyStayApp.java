60d6db944389df2385dac20ca2e92feb9c1c0ca8
DiyaMenon61 <dm6267@srmist.edu.in>
2026-03-16 15:54:29 +0530

    UC4

--- BookMyStayApp.java
+++ BookMyStayApp.java
@@ -1,3 +1,28 @@
+import java.util.HashMap;
+
 public class BookMyStayApp {
 
+    public static void main(String[] args) {
+
+        System.out.println("Book My Stay App - Version 4.0");
+
+        // Centralized inventory
+        HashMap<String, Integer> inventory = new HashMap<>();
+
+        inventory.put("Single Room", 5);
+        inventory.put("Double Room", 3);
+        inventory.put("Suite Room", 0); // not available
+
+        System.out.println("Available Rooms:");
+
+        // Search and display only available rooms
+        for (String room : inventory.keySet()) {
+
+            int available = inventory.get(room);
+
+            if (available > 0) {
+                System.out.println(room + " | Price: Rs.2000 | Available: " + available);
+            }
+        }
+    }
 }
