a53149afc41ea6a57e9cacf09ff6fdbfa99436ff
DiyaMenon61 <dm6267@srmist.edu.in>
2026-03-16 16:01:55 +0530

    UC5

--- BookMyStayApp.java
+++ BookMyStayApp.java
@@ -1,3 +1,21 @@
+import java.util.*;
+
 public class BookMyStayApp {
 
+    public static void main(String[] args) {
+
+        System.out.println("Book My Stay App");
+
+        Queue<String> q = new LinkedList<>();
+
+        q.add("Guest1 - Single Room");
+        q.add("Guest2 - Double Room");
+        q.add("Guest3 - Suite Room");
+
+        System.out.println("Booking Requests:");
+
+        while(!q.isEmpty()) {
+            System.out.println(q.remove());
+        }
+    }
 }
